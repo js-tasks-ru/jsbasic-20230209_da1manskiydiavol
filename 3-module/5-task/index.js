7182c14bf3907dbd18338b0997b8bba48dceead0
Da1manDS <da1manskiydiavol@gmail.com>
2023-02-20 20:03:53 +0300

    3-5. Описана функция нахождения из строки максимального и минимального числа

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,16 @@
 function getMinMax(str) {
-  // ваш код...
+  let _tmpArr = str.split(' ');
+  let _min = 0;
+  let _max = 0;
+
+  for (item of _tmpArr) {
+    item = Number.parseFloat(item);
+    if (_min > item) _min = item;
+    if (_max < item) _max = item;
+  }
+
+  return {
+    min: _min,
+    max: _max,
+  };
 }
